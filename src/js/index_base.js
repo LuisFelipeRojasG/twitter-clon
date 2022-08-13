2026ad9abe5665bff6158159ce92585951b2641f
LuisFelipeRojas <luisfeliperojas27@yahoo.es>
2022-08-12 22:37:33 -0500

    Se reubica el index.html más afuera

--- src/js/index_base.js
+++ src/js/index_base.js
@@ -4,15 +4,15 @@ const winLoginPhone = document.getElementById('loginPhone');
 
 
 winLoginGoogle.addEventListener("click", () => {
-    window.open("./pages/googleLogin.html", "ventana1", "width=510,height=560,scrollbars=NO");
+    window.open("./src/pages/googleLogin.html", "ventana1", "width=510,height=560,scrollbars=NO");
 });
 
 winLoginApple.addEventListener("click", () => {
-    window.open("./pages/appleLogin.html", "ventana1", "width=700,height=658,scrollbars=NO");
+    window.open("./src/pages/appleLogin.html", "ventana1", "width=700,height=658,scrollbars=NO");
 })
 
 winLoginPhone.addEventListener("click", () => {
-    window.open("./pages/phoneLogin.html", "ventana1", "width=620,height=590,scrollbars=NO");
+    window.open("./src/pages/phoneLogin.html", "ventana1", "width=620,height=590,scrollbars=NO");
 })
 
 
